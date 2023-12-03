ad2baefd559b296fcf2bbb5c0bc3997d926c1b67
thesunshade <82448383+thesunshade@users.noreply.github.com>
2023-12-03 17:08:43 +1300

    Update index.js

--- index.js
+++ index.js
@@ -27,25 +27,32 @@ fetch(`https://suttacentral.net/api/languages`)
   });
 
 // file dropdown
-const filesDropdown = document.getElementById("files");
-const filesDescription = document.getElementById("file-description");
-const files = {
-  interface: "most of the website interface",
-  home: "homepage specific content",
-  licensing: `for the <a href="https://suttacentral.net/licensing">Copyright page</a>`,
-};
-const fileKeys = Object.keys(files);
-for (let i = 0; i < fileKeys.length; i++) {
-  filesDropdown.innerHTML += `<option value="${fileKeys[i]}">${fileKeys[i]}</options>`;
-}
-filesDescription.innerHTML = `This file is ${files[fileKeys[0]]}`;
-fileName = fileKeys[1];
+function buildFileDropdown() {
+  const filesDropdown = document.getElementById("files");
+  const filesDescription = document.getElementById("file-description");
+  const files = {
+    interface: "most of the website interface",
+    home: "homepage specific content",
+    about: `for the <a href="https://suttacentral.net/about" rel="noreferrer" target="_blank">About page</a>`,
+    introduction: `for the <a href="https://suttacentral.net/introduction"rel="noreferrer" target="_blank">Introduction to SuttaCentral page</a>`,
+    licensing: `for the <a href="https://suttacentral.net/licensing"rel="noreferrer" target="_blank">Copyright page</a>`,
+    start: `for the <a href="https://suttacentral.net/start"rel="noreferrer" target="_blank">Getting started reading suttas page</a>`,
+  };
+  const fileKeys = Object.keys(files);
+  for (let i = 0; i < fileKeys.length; i++) {
+    filesDropdown.innerHTML += `<option value="${fileKeys[i]}">${fileKeys[i]}</options>`;
+  }
+  filesDescription.innerHTML = `This file is ${files[fileKeys[0]]}`;
+  fileName = fileKeys[1];
 
-filesDropdown.addEventListener("change", e => {
-  const selected = e.target.value;
-  filesDescription.innerHTML = `This file is ${files[selected]}`;
-  fileName = selected;
-});
+  filesDropdown.addEventListener("change", e => {
+    const selected = e.target.value;
+    filesDescription.innerHTML = `This file is ${files[selected]}`;
+    fileName = selected;
+  });
+}
+
+buildFileDropdown();
 
 const submitLanguageButton = document.getElementById("submit-language");
 submitLanguageButton.addEventListener("click", e => {
@@ -53,11 +60,11 @@ submitLanguageButton.addEventListener("click", e => {
 });
 
 allLanguageButton.addEventListener("click", () => {
-  noTranslationsArea.innerHTML = `<h2>The following have no translations of the main interface file:</h2>`;
+  noTranslationsArea.innerHTML = `<h2>The following have no translations of the <code>${fileName}</code> file:</h2>`;
   const languageIds = Object.keys(languageObject);
   for (let i = 0; i < languageIds.length; i++) {
     const languageId = languageIds[i];
-    const target = fetch(`https://raw.githubusercontent.com/suttacentral/bilara-data/published/translation/${languageId}/site/interface_translation-${languageId}-site.json`)
+    const target = fetch(`https://raw.githubusercontent.com/suttacentral/bilara-data/published/translation/${languageId}/site/${fileName}-${languageId}-site.json`)
       .then(response => response.json())
       .then(data => {})
       .catch(error => {
